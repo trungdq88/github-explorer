557d1f7cacf5ce6f34a896efde27f0ab04498820
Trung Dinh Quang <trungdq88@gmail.com>
2016-06-24 18:09:51 +0700

    Profile section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,6 +32,39 @@ export default () => (
 
       </div>
 
+      <div id="user-bio">
+        “Write code, save the world.”
+      </div>
+
+      <div id="user-stats">
+        <div className="stats-divider space-holder"></div>
+
+        <div className="stats-block">
+            <div className="stats-title">20</div>
+            <div className="stats-description">Followers</div>
+        </div>
+
+        <div className="stats-divider"></div>
+
+        <div className="stats-block">
+            <div className="stats-title">141</div>
+            <div className="stats-description">Starred</div>
+        </div>
+
+        <div className="stats-divider"></div>
+
+        <div className="stats-block">
+            <div className="stats-title">10</div>
+            <div className="stats-description">Following</div>
+        </div>
+
+        <div className="stats-divider space-holder"></div>
+      </div>
+
+      <div id="view-repos">
+        <a href="#" className="green-btn">VIEW REPOSITORIES</a>
+      </div>
+
     </div>
   </div>
 );
