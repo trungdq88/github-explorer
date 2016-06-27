9cd4309c4ad639b77a7105eb8eaabba2ad15b554
Trung Dinh Quang <trungdq88@gmail.com>
2016-06-27 13:43:02 +0700

    Footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import './App.less';
 import NavBar from './NavBar/NavBar.jsx';
 import Profile from './Profile/Profile.jsx';
 import PopularRepo from './PopularRepo/PopularRepo.jsx';
+import Footer from './Footer/Footer.jsx';
 
 export default () => (
   <div>
     <NavBar />
     <Profile />
     <PopularRepo />
+    <Footer />
   </div>
 );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.less';
+
+export default () => (
+  <div id="footer">
+    <div id="footer-logo">
+      <i className="fa fa-github"></i>
+    </div>
+    <div id="footer-credit">
+      GitHub concept by<br/>
+      Quan &amp; Bang
+    </div>
+  </div>
+);
