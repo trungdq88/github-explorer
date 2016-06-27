a0dc97e1cb344710fcb75354d2f571d798c1e592
Trung Dinh Quang <trungdq88@gmail.com>
2016-06-27 13:45:52 +0700

    NavBar -> Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.less';
-import NavBar from './NavBar/NavBar.jsx';
+import Header from './Header/Header.jsx';
 import Profile from './Profile/Profile.jsx';
 import PopularRepo from './PopularRepo/PopularRepo.jsx';
 import Footer from './Footer/Footer.jsx';
 
 export default () => (
   <div>
-    <NavBar />
+    <Header />
     <Profile />
     <PopularRepo />
     <Footer />
