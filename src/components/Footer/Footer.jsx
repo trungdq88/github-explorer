a96541036f7e91147671dce8cc648ce2b11065ae
Trung Dinh Quang <trungdq88@gmail.com>
2016-06-28 15:45:29 +0700

    Add version number

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,9 @@ export default () => (
   <div id="footer">
     <div id="footer-logo">
       <i className="fa fa-github"></i>
+      <span id="version">
+        {window.VERSION}
+      </span>
     </div>
     <div id="footer-credit">
       GitHub concept by<br />
