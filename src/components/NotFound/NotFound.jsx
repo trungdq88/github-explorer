f52455ae77157ef251a152c005e820eb5f138728
Trung Dinh Quang <trungdq88@gmail.com>
2016-07-08 15:00:53 +0700

    Build

--- src/components/NotFound/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.less';
-
-export default () => (
-  <div id="not-found">
-    404 - Page not found
-  </div>
-);
--- src/tests.js
+++
@@ -1,4 +0,0 @@
-import 'babel-polyfill';
-
-const context = require.context('.', true, /-test\.jsx?$/);
-context.keys().forEach(context);
